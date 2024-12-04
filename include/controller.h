2473ca9a464b53c5693835965d08c7a62796b7e9
Yannick Suter <yannick.suter@gmail.com>
2024-12-04 09:10:53 +0100

    basic code to test library integration into platform.io and esphome projects

---
+++ include/controller.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace siebenuhr 
+{
+    class Controller 
+    {
+        Controller() = default;
+        ~Controller() = default;
+
+        void initialize();
+    };
+}
---
+++ src/controller.cpp
@@ -0,0 +1,9 @@
+#include "controller.h"
+
+namespace siebenuhr 
+{
+    void Controller::initialize() 
+    {
+        // just some basic code to test the library integration
+    }
+}
