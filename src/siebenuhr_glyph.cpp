42383005595b761fe85e23570d29bf4439cc629e
Yannick Suter <yannick.suter@gmail.com>
2025-01-13 22:26:54 +0100

    adding digit and ascii char rendering to glyph

--- src/siebenuhr_glyph.cpp
+++ src/siebenuhr_glyph.cpp
@@ -3,17 +3,16 @@
 namespace siebenuhr_core
 {
     Glyph::Glyph(int numSegments, int numLEDsPerSegments)
-        : m_numSegments(numSegments)
-        , m_numLEDsPerSegments(numLEDsPerSegments)
+        : m_numSegments(numSegments), m_numLEDsPerSegments(numLEDsPerSegments)
 
     {
         m_numLEDS = m_numSegments * m_numLEDsPerSegments;
     }
 
-    void Glyph::attach(int glyphID, int glyphCount) 
+    void Glyph::attach(int glyphID, int glyphCount)
     {
         m_glyphID = glyphID;
-        m_glyphOffset = (glyphCount-m_glyphID-1)*m_numSegments*m_numLEDsPerSegments;
+        m_glyphOffset = (glyphCount - m_glyphID - 1) * m_numSegments * m_numLEDsPerSegments;
     }
 
     void Glyph::setEffect(Effect *effect)
@@ -24,9 +23,34 @@ namespace siebenuhr_core
 
     void Glyph::update(unsigned long currentMillis, CRGB *LEDs)
     {
-        if (m_effect != nullptr) 
+        if (m_effect != nullptr)
         {
             m_effect->update(currentMillis, LEDs);
+        }
+        else
+        {
+            char_id = (char_id+1)%127;
+
+            for (size_t i = 0; i < m_numSegments * m_numLEDsPerSegments; ++i) 
+            {
+                LEDs[m_glyphOffset + i].r = 0;
+                LEDs[m_glyphOffset + i].g = 0;
+                LEDs[m_glyphOffset + i].b = 0;
+            }
+
+            for (size_t i = 0; i < m_numSegments; ++i) 
+            {
+                if (ASCII_TABLE[char_id][i] == 1)
+                {
+                    int segmentOffset = i*m_numLEDsPerSegments;
+                    for (size_t e = 0; e < m_numLEDsPerSegments; ++e) 
+                    {
+                        LEDs[m_glyphOffset + i].r = 255;
+                        LEDs[m_glyphOffset + i].g = 255;
+                        LEDs[m_glyphOffset + i].b = 255;
+                    }                    
+                }
+            }
         }
     }
 
